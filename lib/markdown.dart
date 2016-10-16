d479524a6d84d9676d64f8a07747178fc10aca66
kyorohiro <kyorohiro@gmail.com>
2016-10-17 04:03:44 +0900

    ..

--- lib/markdown.dart
+++ lib/markdown.dart
@@ -5,72 +5,128 @@ import 'dart:async';
 import 'dart:convert' as conv;
 
 class Markdown {
-  int sharp = 0x23; //#
-  int space = 0x20; //space
-  int cr = 0x0d;
-  int lf = 0x0a;
+  static int sharp = 0x23; //#
+  static int space = 0x20; //space
+  static int asterisk = 0x2a;
+  static int cr = 0x0d;
+  static int lf = 0x0a;
   par.MiniParser parser;
-  Exception defaultError = new Exception();
+  static Exception defaultError = new Exception();
 
   Markdown(par.Reader src) {
     this.parser = new par.MiniParser(src);
   }
 
-  Future<GObject> italic() async {
-    return null;
-  }
-
-  Future<int> headingSharp() async {
-    int sharpNum = 0;
-    try {
-      while (true) {
-        await parser.nextByte(sharp);
-        sharpNum++;
-      }
-    } catch (e) {}
-    return sharpNum;
-  }
-
-  Future<int> headingSpace() async {
-    int spaceNum = 0;
-    try {
-      while (true) {
-        await parser.nextByte(space);
-        spaceNum++;
-      }
-    } catch (e) {}
-    return spaceNum;
+  Future<GObject> encodeAll() async {
+    return SourceObject.source(parser);
   }
 
   Future<GObject> heading() async {
-    parser.push();
-    int numOfSharp = 0;
+    return HeadObject.heading(parser);
+  }
+
+  Future<GObject> strong() async {
+    return StrongObject.em(parser);
+  }
+}
+
+class SourceObject extends GObject {
+  List<GObject> content = [];
+  SourceObject() {}
+  String toString() {
+    return "";
+  }
+
+  static Future<GObject> source(par.MiniParser parser) async {
+    SourceObject ret = new SourceObject();
+    while (true) {
+      try {
+        ret.content.add(await StrongObject.em(parser));
+        continue;
+      } catch (e) {}
+
+      try {
+        ret.content.add(await HeadObject.heading(parser));
+        continue;
+      } catch (e) {}
+
+      int v = await parser.readByte();
+      ret.content.add(new TextObject([v]));
+      break;
+    }
+    return ret;
+  }
+}
+
+class TextObject extends GObject {
+  List<int> cont;
+  TextObject(this.cont) {}
+}
+
+class StrongObject extends GObject {
+  List<int> content;
+
+  StrongObject(this.content) {}
+  String toString() {
+    return "<em>${conv.UTF8.decode(content,allowMalformed: true)}</em>";
+  }
+
+  static Future<GObject> em(par.MiniParser parser) async {
+    List<int> cont = [];
     try {
-      numOfSharp = await headingSharp();
-      if (numOfSharp == 0) {
-        throw defaultError;
+      parser.push();
+      if (false == await emStart(parser)) {
+        throw Markdown.defaultError;
       }
-      int numOfSpace = await headingSpace();
-      if (numOfSpace == 0) {
-        parser.back();
-        throw defaultError;
+
+      while (true) {
+        if (true == await emEnd(parser)) {
+          break;
+        }
+        //
+        var v = await parser.readByte();
+        cont.add(v);
       }
+    } catch (e) {
+      parser.back();
+      throw e;
     } finally {
       parser.pop();
     }
-    //
-    List<int> ret = [];
-    try {
-      while (true) {
-        var v = await parser.readByte();
-        ret.add(v);
-        if (v == lf) {
-          break;
-        }
-      }
-    } catch (e) {} finally {}
+    return new StrongObject(cont);
+  }
 
-    return new HeadObject(numOfSharp, ret);
+  static Future<bool> emStart(par.MiniParser parser) async {
+    bool ret = false;
+    try {
+      parser.push();
+      await parser.nextBytes([Markdown.asterisk, Markdown.asterisk]);
+      if (Markdown.space == await parser.readByte()) {
+        throw Markdown.defaultError;
+      }
+      ret = true;
+    } catch (e) {
+      parser.back();
+      ret = false;
+    } finally {
+      parser.pop();
+    }
+    return ret;
+  }
+
+  static Future<bool> emEnd(par.MiniParser parser) async {
+    bool ret = false;
+    try {
+      parser.push();
+      await parser.nextBytes([Markdown.asterisk, Markdown.asterisk]);
+      ret = true;
+    } catch (e) {
+      parser.back();
+      ret = false;
+    } finally {
+      parser.pop();
+    }
+    return ret;
   }
 }
 
@@ -81,6 +137,62 @@ class HeadObject extends GObject {
   HeadObject(this.id, this.content) {}
   String toString() {
     return "<h${this.id}>${conv.UTF8.decode(content,allowMalformed: true)}</h${this.id}>";
+  }
+
+  static Future<GObject> heading(par.MiniParser parser) async {
+    parser.push();
+    int numOfSharp = 0;
+    try {
+      numOfSharp = await headingSharp(parser);
+      if (numOfSharp == 0) {
+        throw Markdown.defaultError;
+      }
+      int numOfSpace = await headingSpace(parser);
+      if (numOfSpace == 0) {
+        parser.back();
+        throw Markdown.defaultError;
+      }
+    } finally {
+      parser.pop();
+    }
+    //
+    List<int> ret = [];
+    try {
+      while (true) {
+        var v = await parser.readByte();
+        ret.add(v);
+        if (v == Markdown.lf) {
+          break;
+        }
+      }
+    } catch (e) {} finally {}
+
+    return new HeadObject(numOfSharp, ret);
+  }
+
+  //
+  //
+  //
+  static Future<int> headingSharp(par.MiniParser parser) async {
+    int sharpNum = 0;
+    try {
+      while (true) {
+        await parser.nextByte(Markdown.sharp);
+        sharpNum++;
+      }
+    } catch (e) {}
+    return sharpNum;
+  }
+
+  static Future<int> headingSpace(par.MiniParser parser) async {
+    int spaceNum = 0;
+    try {
+      while (true) {
+        await parser.nextByte(Markdown.space);
+        spaceNum++;
+      }
+    } catch (e) {}
+    return spaceNum;
   }
 }
 
---
+++ test/emtest.dart
@@ -0,0 +1,14 @@
+import 'package:test/test.dart' as unit;
+import 'package:firemarkdown/markdown.dart' as mar;
+import 'package:miniparser/core.dart' as par;
+import 'dart:convert' as conv;
+
+void main() {
+  unit.test("ps2", () async {
+    var src = new par.BytesReader.fromList(conv.UTF8.encode("**asdfasdfasdfasd**"),isImmutable: true);
+    mar.Markdown markdownObj = new mar.Markdown(src);
+    var gobj = await markdownObj.strong();
+    print(">>> ${gobj}");
+//    unit.expect(encode, "hello");
+  });
+}
